918a1112d9b1d841813de7616042c85ca57815ea
KimEunHye <gpqjr00@naver.com>
2025-02-19 03:30:10 +0900

    Feat: 기본적인 라우터 페이지 생성

---
+++ src/app/(splash)/splash/page.tsx
@@ -0,0 +1,12 @@
+import Background from '@/components/Background';
+import Contents from '@/components/Contents';
+
+export default function Splash() {
+  return (
+    <Background>
+      <Contents>
+        <div className="text-red">splash</div>
+      </Contents>
+    </Background>
+  );
+}
