44afc6bc27751ebf90d335166bda3e3f12e3bacb
KimEunHye <gpqjr00@naver.com>
2025-02-25 01:54:49 +0900

    Feat: Splash 화면 구성 완료

--- src/app/(splash)/splash/page.tsx
+++ src/app/(splash)/splash/page.tsx
@@ -1,11 +1,15 @@
 import Background from '@/app/components/Background';
 import Contents from '@/app/components/Contents';
+import Image from 'next/image';
+
+import Logo from '@/asset/PayTimeLogo.png';
 
 export default function Splash() {
   return (
     <Background>
-      <Contents>
-        <div className="text-red">splash</div>
+      <Contents splash={1}>
+        <Image src={Logo} width={316} height={316} alt="페이타임 로고" />
+        <div className="text-5xl mt-2">PayTime</div>
       </Contents>
     </Background>
   );
