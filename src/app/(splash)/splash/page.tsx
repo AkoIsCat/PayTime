d7b873123d757d667fb982b257a7173c9c973719
KimEunHye <gpqjr00@naver.com>
2025-02-27 00:19:43 +0900

    Feat: 1.2초 후 메인페이지로 이동되도록 구현

--- src/app/(splash)/splash/page.tsx
+++ src/app/(splash)/splash/page.tsx
@@ -1,7 +1,21 @@
+'use client';
 import Background from '@/app/components/Background';
 import Contents from '@/app/components/Contents';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 export default function Splash() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/main');
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <Background>
       <Contents>
