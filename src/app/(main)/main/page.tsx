8b6b4cf430608616adbcdf4b231dd910acfa12b5
KimEunHye <gpqjr00@naver.com>
2025-03-04 00:03:04 +0900

    Feat: 메인페이지 필요 컴포넌트 추가

--- src/app/(main)/main/page.tsx
+++ src/app/(main)/main/page.tsx
@@ -1,13 +1,33 @@
+'use client';
 import Background from '@/app/components/Background';
 import Contents from '@/app/components/Contents';
+import Header from '../components/Header';
 
-import { MergeChildrenSplash } from '@/types';
+import SalaryResultBox from '../components/SalaryResultsBox';
+import { FormEvent } from 'react';
+import HourlyWageInfo from '../components/HourlyWageInfo';
+import WorkDetailsForm from '../components/WorkDetailsForm';
 
-export default function Main({ splash, children }: MergeChildrenSplash) {
+export default function Main() {
+  const onSubmit = (
+    e: FormEvent<HTMLFormElement>,
+    formData: (number | boolean | string)[]
+  ) => {
+    e.preventDefault();
+    console.log('계산하기', formData);
+  };
+
   return (
     <Background>
       <Contents splash={0}>
-        <div>메인 페이지</div>
+        <div>
+          <Header />
+          <HourlyWageInfo year={2025} hourlyWage={10030} />
+          <WorkDetailsForm onSubmit={onSubmit} />
+          <SalaryResultBox />
+          <SalaryResultBox />
+          <SalaryResultBox />
+        </div>
       </Contents>
     </Background>
   );
