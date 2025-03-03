06d47ce79e86d24e9b999fcfc2f4cae6989179cc
KimEunHye <gpqjr00@naver.com>
2025-03-03 21:18:00 +0900

    Feat: 메인 페이지 틀 추가

--- src/app/(main)/main/page.tsx
+++ src/app/(main)/main/page.tsx
@@ -1,3 +1,14 @@
-export default function Main() {
-  return <div>main</div>;
+import Background from '@/app/components/Background';
+import Contents from '@/app/components/Contents';
+
+import { MergeChildrenSplash } from '@/types';
+
+export default function Main({ splash, children }: MergeChildrenSplash) {
+  return (
+    <Background>
+      <Contents splash={0}>
+        <div>메인 페이지</div>
+      </Contents>
+    </Background>
+  );
 }
