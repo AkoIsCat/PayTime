d79cb02ff91b8db0be76d0ba875e41a680be4a26
KimEunHye <gpqjr00@naver.com>
2025-03-05 03:15:34 +0900

    Feat: 컴포넌트 확인을 위한 코드

--- src/app/(main)/main/page.tsx
+++ src/app/(main)/main/page.tsx
@@ -1,3 +1,21 @@
+import Background from '@/app/components/Background';
+import Contents from '@/app/components/Contents';
+import HourlyWageForm from '../components/HourlyWageForm';
+import SelectWorkHours from '../components/SelectWorkHours';
+import DetailHoursToggle from '../components/DetailHoursToggle';
+
 export default function Main() {
-  return <div>main</div>;
+  return (
+    <Background>
+      <Contents splash={0}>
+        <div>
+          <HourlyWageForm />
+          <DetailHoursToggle>상세하게 입력</DetailHoursToggle>
+          <SelectWorkHours itemType="time" />
+          <DetailHoursToggle>상세하게 입력</DetailHoursToggle>
+          <SelectWorkHours itemType="day" />
+        </div>
+      </Contents>
+    </Background>
+  );
 }
