3be74a353fec76b79d3ee9f8d60d3ac4de41b598
KimEunHye <gpqjr00@naver.com>
2025-03-05 03:15:17 +0900

    Feat: 타입 추가 및 주석 작성

--- src/types.ts
+++ src/types.ts
@@ -4,14 +4,28 @@ export interface Children {
   children: React.ReactNode;
 }
 
+// 버튼의 필요 type
 export interface ButtonType {
-  btnType: 'submit' | 'clear';
+  btnType: 'submit' | 'clear'; // 전송 | 초기화
   onClick: () => {};
 }
 
+// 콘텐츠 영역 필요 type
 export interface ContentsType {
   splash: 0 | 1; // splash 페이지면 1, 아니면 0
 }
 
+// form 항목들 label의 필요 type
+export interface LabelForType {
+  htmlFor: string;
+}
+
+// select 아이템의 type
+export interface SelectType {
+  itemType: 'time' | 'day';
+}
+
 // Children과 ContentsType을 병합
 export type MergeChildrenSplash = Children & ContentsType;
+
+export type MergeChildrenFor = Children & LabelForType;
