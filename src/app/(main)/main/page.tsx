cab44f116e97fcbb2a9058f83e3b2f6e8e2429c4
KimEunHye <gpqjr00@naver.com>
2025-03-10 23:54:45 +0900

    Feat: 컴포넌트 테스트

--- src/app/(main)/main/page.tsx
+++ src/app/(main)/main/page.tsx
@@ -5,6 +5,10 @@ import HourlyWageForm from '../components/HourlyWageForm';
 import SelectWorkHours from '../components/SelectWorkHours';
 import DetailHoursToggle from '../components/DetailHoursToggle';
 import CustomButton from '../components/Button';
+import SalaryResultsBox from '../components/SalaryResultsBox';
+import SalarySelectForm from '../components/SalarySelectForm';
+import AddDetailIButton from '../components/AddDetailIButton';
+import DetailInputForm from '../components/DetailInputForm';
 
 export default function Main() {
   return (
@@ -12,6 +16,9 @@ export default function Main() {
       <Contents splash={0}>
         <div>
           <HourlyWageForm />
+          <SalarySelectForm type="start" />
+          <SalarySelectForm type="end" />
+          <DetailInputForm />
           <DetailHoursToggle>상세하게 입력</DetailHoursToggle>
           <SelectWorkHours itemType="time" />
           <DetailHoursToggle>상세하게 입력</DetailHoursToggle>
@@ -19,11 +26,16 @@ export default function Main() {
           <DetailHoursToggle>상세하게 입력</DetailHoursToggle>
           <SelectWorkHours itemType="tax" />
         </div>
+        <AddDetailIButton onClick={() => console.log('+')} />
         <CustomButton
           btnType="submit"
           onClick={() => console.log('계산하기')}
         />
         <CustomButton btnType="reset" onClick={() => console.log('초기화')} />
+        <SalaryResultsBox resultsType="day" salary={100000} />
+        <SalaryResultsBox resultsType="WH" salary={100000} />
+        <SalaryResultsBox resultsType="week" salary={100000} />
+        <SalaryResultsBox resultsType="month" salary={100000} />
       </Contents>
     </Background>
   );
