a5509e38b380422e21908b0ec0cd3f8361352ea9
KimEunHye <gpqjr00@naver.com>
2025-03-21 01:58:17 +0900

    Feat: form 관련 state 및 action 추가

--- src/store/form.ts
+++ src/store/form.ts
@@ -3,20 +3,61 @@ import { create } from 'zustand';
 type State = {
   salarySelected: string;
   detailToggle: boolean;
+  detailForm: { id: string; day: string; time: string }[];
 };
 
 type Actions = {
   selectedSalaryOption: (selectedSalary: string) => void;
   detailToggleHandle: () => void;
+  resetToggleHandle: () => void;
+  addDetailForm: () => void;
+  updateForm: (id: string, day: string, time: string) => void;
+  removeForm: (id: string) => void;
+  resetForm: () => void;
 };
 
 export const useFormStore = create<State & Actions>((set) => ({
   salarySelected: 'month',
   detailToggle: false,
+  detailForm: [],
   selectedSalaryOption: (selectedSalary: string) => {
     set({ salarySelected: selectedSalary });
   },
   detailToggleHandle: () => {
     set((state) => ({ detailToggle: !state.detailToggle }));
+  },
+  resetToggleHandle: () => {
+    set({ detailToggle: false });
+  },
+  addDetailForm: () => {
+    set((state) => {
+      if (state.detailForm.length >= 7) {
+        return state;
+      }
+      const newValue = { id: Date.now().toString(), day: '', time: '0' };
+      return {
+        ...state,
+        detailForm: [...state.detailForm, newValue],
+      };
+    });
+  },
+  updateForm: (id: string, day?: string, time?: string) => {
+    set((state) => ({
+      ...state,
+      detailForm: state.detailForm.map((item) =>
+        item.id === id
+          ? { ...item, day: day ?? item.day, time: time ?? item.time }
+          : item
+      ),
+    }));
+  },
+  removeForm: (id: string) => {
+    set((state) => ({
+      ...state,
+      detailForm: state.detailForm.filter((item) => item.id !== id),
+    }));
+  },
+  resetForm: () => {
+    set({ detailForm: [] });
   },
 }));
