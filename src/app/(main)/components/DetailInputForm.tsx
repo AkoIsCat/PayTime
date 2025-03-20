a83006f845c76a7a26dbdac77b4f6e8dfcc0eb5d
KimEunHye <gpqjr00@naver.com>
2025-03-21 02:01:23 +0900

    Feat: 상세 폼 데이터 입력 관련 코드 추가

--- src/app/(main)/components/DetailInputForm.tsx
+++ src/app/(main)/components/DetailInputForm.tsx
@@ -1,14 +1,45 @@
 import SelectWorkHours from './SelectWorkHours';
 import DetailDaySelect from './DetailDaySelect';
 
-export default function DetailInputForm() {
+import { Id } from '@/types';
+import { useEffect, useState } from 'react';
+import { useFormStore } from '@/store/form';
+import RemoveDetailForm from './RemoveDetailForm';
+
+export default function DetailInputForm({ id }: Id) {
+  const { updateForm } = useFormStore();
+  const [value, setValue] = useState<{
+    id: string;
+    day?: string;
+    time?: string;
+  }>({ id });
+
+  const dayValue = (day: string) => {
+    setValue((state) => ({
+      ...state,
+      day,
+    }));
+  };
+
+  const timeValue = (time: string) => {
+    setValue((state) => ({
+      ...state,
+      time,
+    }));
+  };
+
+  useEffect(() => {
+    updateForm(value.id, value.day ?? '', value.time ?? '');
+  }, [value]);
+
   return (
-    <div className="flex">
+    <div className="flex" id={id}>
       <div className="flex text-sm mx-2 text-center items-center">
-        <DetailDaySelect />
+        <DetailDaySelect getDay={dayValue} />
         <p className="mx-1 w-days">요일</p>
       </div>
-      <SelectWorkHours itemType="time" />
+      <SelectWorkHours itemType="time" getTime={timeValue} />
+      <RemoveDetailForm id={value.id} />
     </div>
   );
 }
