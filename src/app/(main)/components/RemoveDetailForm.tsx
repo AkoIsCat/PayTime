5d3725bf7aa7aa79f449593251e400d27ed0841b
KimEunHye <gpqjr00@naver.com>
2025-04-01 23:19:28 +0900

    Feat: 세부 입력 폼 삭제 시 중복 제거 배열에 업데이트 되도록 추가

--- src/app/(main)/components/RemoveDetailForm.tsx
+++ src/app/(main)/components/RemoveDetailForm.tsx
@@ -1,15 +1,25 @@
-import { Id } from '@/types';
+import { MergeDetailId } from '@/types';
 import { useFormStore } from '@/store/form';
 import { IoMdRemove } from 'react-icons/io';
 
-export default function RemoveDetailForm({ id }: Id) {
+export default function RemoveDetailForm({
+  id,
+  setSelectedDays,
+}: MergeDetailId) {
   const { removeForm } = useFormStore();
 
   return (
     <button
       type="button"
       id={id}
-      onClick={() => removeForm(id)}
+      onClick={() => {
+        removeForm(id);
+        setSelectedDays((prev) => {
+          const updated = { ...prev };
+          delete updated[id];
+          return updated;
+        });
+      }}
       className="text-2xl w-12 h-8 font-bold text-red-700 rounded-full my-2 mx-1 hover:bg-red-100 flex justify-center items-center"
     >
       <IoMdRemove />
