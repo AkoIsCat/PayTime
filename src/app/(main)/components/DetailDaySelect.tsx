acb9ab7e37e0cb23eef2afa5df3af44aa24804ce
KimEunHye <gpqjr00@naver.com>
2025-04-01 23:17:34 +0900

    Feat: 세부입력 폼 요일 선택시 이미 선택된 요일은 고르지 못하도록 추가

--- src/app/(main)/components/DetailDaySelect.tsx
+++ src/app/(main)/components/DetailDaySelect.tsx
@@ -8,10 +8,29 @@ import {
 import { cn } from '@/lib/utils';
 import { DetailDayType } from '@/types';
 
-export default function DetailDaySelect({ getDay }: DetailDayType) {
+export default function DetailDaySelect({
+  id,
+  getDay,
+  selectDays,
+  setSelectedDays,
+}: DetailDayType) {
   const days = ['월', '화', '수', '목', '금', '토', '일'];
+
+  // 사용자가 선택중인 요일
+  const usedDays = Object.entries(selectDays)
+    .filter(([key]) => key !== id)
+    .map(([, day]) => day);
+
+  // 사용자가 선택 가능한 요일
+  const availableDays = days.filter((d) => !usedDays.includes(d));
+
   return (
-    <Select onValueChange={(value) => getDay(value)}>
+    <Select
+      onValueChange={(value) => {
+        getDay(value);
+        setSelectedDays((prev) => ({ ...prev, [id]: value }));
+      }}
+    >
       <SelectTrigger
         className={cn(
           '[&>svg]:hidden w-dayInput h-select m-0 p-0 border-b border-t-0 border-l-0 border-r-0 rounded-none border-black flex justify-center'
@@ -20,7 +39,7 @@ export default function DetailDaySelect({ getDay }: DetailDayType) {
         <SelectValue placeholder="선택" className={cn('m-0 p-0 text-[10px]')} />
       </SelectTrigger>
       <SelectContent className={cn('bg-white')}>
-        {days.map((item) => (
+        {availableDays.map((item) => (
           <SelectItem key={item} value={item} className={cn('cursor-pointer')}>
             {item}
           </SelectItem>
