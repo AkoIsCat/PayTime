d5e5248d7585c1200e31606922b16a69d733dafb
KimEunHye <gpqjr00@naver.com>
2025-04-08 02:47:36 +0900

    Feat: 연장, 야간, 휴일 수당 계산 추가 및 state를 불러올 시 number 타입으로 바로 변환되도록 추가

--- src/app/(main)/main/page.tsx
+++ src/app/(main)/main/page.tsx
@@ -17,11 +17,19 @@ export default function Main() {
   const [monthSalary, setMonthSalary] = useState<number>(0);
 
   const salarySelected = useFormStore((state) => state.salarySelected);
-  const dailyWorkingHours = useFormStore((state) => state.dailyWorkingHours);
-  const hourlyWage = useFormStore((state) => state.hourlyWage);
-  const weeklyWorkDays = useFormStore((state) => state.weeklyWorkDays);
-  const weeklyAllowance = useFormStore((state) => state.weeklyAllowance);
+  const dailyWorkingHours = useFormStore((state) => +state.dailyWorkingHours);
+  const hourlyWage = useFormStore((state) => +state.hourlyWage);
+  const weeklyWorkDays = useFormStore((state) => +state.weeklyWorkDays);
+  const weeklyAllowance = useFormStore((state) => +state.weeklyAllowance);
   const detailForm = useFormStore((state) => state.detailForm);
+  const tax = useFormStore((state) => +state.tax);
+  const nightWorkingHours = useFormStore((state) => +state.nightWorkingHours);
+  const holidayWorkingHours = useFormStore(
+    (state) => +state.holidayWorkingHours
+  );
+  const overTimeWokringHours = useFormStore(
+    (state) => +state.overTimeWorkingHours
+  );
 
   const isCalculated = useFormStore((action) => action.isCalculated);
 
@@ -32,33 +40,49 @@ export default function Main() {
   }, [isCalculated]);
 
   const onClickCalculate = () => {
-    const weekSalary = // 주급
-      +hourlyWage * (+dailyWorkingHours / 60) * +weeklyWorkDays;
-    const weekWorkingHours = (+dailyWorkingHours * +weeklyWorkDays) / 60;
+    const overTimeSalary = (overTimeWokringHours / 60) * (hourlyWage * 1.5);
+    const nightTimeSalary = (nightWorkingHours / 60) * (hourlyWage * 1.5);
+    const holiydaySalary = (holidayWorkingHours / 60) * (hourlyWage * 1.5);
+    const weekSalary = hourlyWage * (dailyWorkingHours / 60) * weeklyWorkDays; // 주급
+    const weekWorkingHours = (dailyWorkingHours * weeklyWorkDays) / 60;
     const WHA = // 주휴수당
       weekWorkingHours > 40
-        ? (+dailyWorkingHours / 60) * +hourlyWage
-        : ((weekWorkingHours * 8) / 40) * +hourlyWage;
+        ? (dailyWorkingHours / 60) * hourlyWage
+        : ((weekWorkingHours * 8) / 40) * hourlyWage;
 
     if (salarySelected) {
-      setDaySalary(+hourlyWage * (+dailyWorkingHours / 60));
+      const salary = hourlyWage * (dailyWorkingHours / 60);
+      setDaySalary(
+        salarySelected === 'day'
+          ? salary + overTimeSalary + nightTimeSalary + holiydaySalary
+          : salary
+      );
     }
     if (salarySelected === 'week' || salarySelected === 'month') {
-      setWeekSalary(weekSalary);
+      setWeekSalary(
+        salarySelected === 'week'
+          ? weekSalary + overTimeSalary + nightTimeSalary + holiydaySalary
+          : weekSalary
+      );
       setWHsalary(WHA);
     }
     if (salarySelected === 'month') {
-      setMonthSalary(weekSalary * 4);
+      const monthSalary =
+        weekSalary * 4 + overTimeSalary + nightTimeSalary + holiydaySalary;
+      setMonthSalary(monthSalary - monthSalary * (tax / 100));
     }
   };
 
-  // 연장, 야간, 휴일은 시급의 1.5배
-  // 야간, 휴일 근무시간 입력 시 일일 근무시간에서 해당되는 시간은 제외 후 입력해야함
-  // 전부 야간(휴일) 근무 시 일일 근무시간 0으로 입력.
-  // 일일 근무시간이 0일 경우 야간(휴일) 근무시간이 입력되어 있지 않으면 경고창 띄우기
+  // 연장, 야간, 휴일은 시급의 1.5배 o
+  // 야간, 휴일 근무시간 입력 시 일일 근무시간에서 해당되는 시간은 제외 후 입력해야함 o
+  // 전부 야간(휴일) 근무 시 일일 근무시간 0으로 입력. o
+  // 일일 근무시간이 0일 경우 야간(휴일) 근무시간이 입력되어 있지 않으면 경고창 띄우기 o
   // 상세 입력 폼에서도 마찬가지. 해당 근무 요일의 시간을 조정해야함.
   // 상세 입력 시 주휴수당은 모든 요일의 시간을 더한 후 근무 일수를 나누고 시급을 곱함
-  // 세금도 급여에서 계산하기
+  // 세금도 급여에서 계산하기 o
+  // 근무시간 입력 폼에서 ! 아이콘으로 야간/휴일 근무시간은 해당 일일 근무시간에서 시간을 제외하도록 유도
+  // 월급 계산 시 주휴수당을 포함하지 않은 금액이라고 안내
+  // 세금 툴팁으로 어떤 종류가 있는지 표시하기(알바천국, 알바몬 참고)
 
   return (
     <Background>
@@ -70,7 +94,7 @@ export default function Main() {
           <SalaryResultsBox resultsType="day" salary={daySalary} />
         )}
         {(salarySelected === 'week' || salarySelected === 'month') &&
-          weeklyAllowance &&
+          !!weeklyAllowance &&
           isCalculated && (
             <SalaryResultsBox resultsType="WH" salary={WHsalary} />
           )}
