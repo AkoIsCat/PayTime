933be3a4e45b0f092d5fb7733ca7d7832e6e9c95
KimEunHye <gpqjr00@naver.com>
2025-04-09 02:34:33 +0900

    Feat: 조건별로 안내 문구를 다르게 표시하도록 작성

--- src/app/(main)/components/FormLabel.tsx
+++ src/app/(main)/components/FormLabel.tsx
@@ -1,18 +1,41 @@
 import { RiErrorWarningLine } from 'react-icons/ri';
+import InfoTooltip from './InfoTooltip';
+
 import { MergeChildrenFor } from '@/types';
+import { useState } from 'react';
 
 export default function FormLabel({
   children,
   htmlFor,
   itemType,
 }: MergeChildrenFor) {
+  const [showTooltip, setShowTooltip] = useState<boolean>(false);
+
+  const hoverIcon = () => {
+    setShowTooltip(true);
+  };
+
+  const leaveIcon = () => {
+    setShowTooltip(false);
+  };
   return (
     <label
-      className="text-black m-0 text-sm flex items-center"
+      className="text-black m-0 text-sm flex items-center relative"
       htmlFor={htmlFor}
     >
       <div className="mr-1">{children}</div>
-      {(itemType === 'tax' || itemType === 'time') && <RiErrorWarningLine />}
+      {(itemType === 'tax' || itemType === 'time') && (
+        <RiErrorWarningLine onMouseEnter={hoverIcon} onMouseLeave={leaveIcon} />
+      )}
+      {showTooltip && (
+        <InfoTooltip
+          text={
+            itemType === 'time'
+              ? '야간/휴일 근무시간은 해당 일일 근무시간에서 시간을 제외해주세요.'
+              : '세금 싫어'
+          }
+        />
+      )}
     </label>
   );
 }
