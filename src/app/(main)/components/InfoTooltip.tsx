d505bf5f8aeb5f77b5da4d3a53b58adb4f566300
KimEunHye <gpqjr00@naver.com>
2025-04-17 05:30:43 +0900

    Remove: 사용하지 않는 컴포넌트 파일 삭제

--- src/app/(main)/components/InfoTooltip.tsx
+++
@@ -1,9 +0,0 @@
-import {  Children } from '@/types';
-
-export default function InfoTooltip({ children }: Children) {
-  return (
-    <div className="absolute left-full w-44 ml-1 p-2 text-sm border bg-white">
-      {children}
-    </div>
-  );
-}
