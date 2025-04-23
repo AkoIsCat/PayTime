c4c3536159f3b1836d65a2173cd853201a77c8d5
KimEunHye <gpqjr00@naver.com>
2025-04-24 01:16:33 +0900

    Design: 급여 결과 모바일 화면 마진 조정

--- src/app/(main)/components/UI/SalaryResultsBox.tsx
+++ src/app/(main)/components/UI/SalaryResultsBox.tsx
@@ -3,8 +3,8 @@ import TooltipInfo from './TootipInfo';
 
 export default function SalaryResultsBox({ resultsType, salary }: ResultsType) {
   return (
-    <div className="flex justify-between w-input p-2 text-xl">
-      <div className="flex items-center relative">
+    <div className="flex justify-between w-full max-w-input p-2 text-xl">
+      <div className="flex items-center relative mx-3">
         <span className="mr-1 inline-block">예상</span>
         <span>
           {resultsType === 'day'
@@ -26,7 +26,7 @@ export default function SalaryResultsBox({ resultsType, salary }: ResultsType) {
           </TooltipInfo>
         )}
       </div>
-      <div>
+      <div className="mx-3">
         <span className="inline-block mr-1">
           {resultsType === 'day'
             ? '일'
